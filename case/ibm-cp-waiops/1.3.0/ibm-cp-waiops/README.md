4bc01f8154ebe20fb938d050e8afe6483a47509c
lyanwei <lyanwei@cn.ibm.com>
2022-07-13 11:11:15 +0800

    add waiops case

---
+++ case/ibm-cp-waiops/1.3.0/ibm-cp-waiops/README.md
@@ -0,0 +1,231 @@
+# Name
+
+IBM Cloud Pak&reg; for Watson AIOps AI Manager v3.4
+
+# Introduction
+
+## Details
+
+IBM Cloud Pak&reg; for Watson AIOps is an AIOps platform that deploys advanced, explainable AI across the IT Operations (ITOps) toolchain so that you can confidently assess, diagnose, and resolve incidents across mission-critical workloads.
+
+Built on IBM Automation foundation, IBM Cloud Pak for Watson AIOps eases the path to adopting advanced AI for ITOps to decrease your operational costs. With this Cloud Pak, you can increase your customer satisfaction by proactively avoiding incidents and accelerating your time to resolution.
+
+The scale of IT systems and their complexity is continually increasing over the last few years because of digital transformation, containerization, and hybrid cloud adoption. IT teams are being inundated with routine maintenance activities and expanding cloud services, leaving them little or no time to contribute toward innovation. To accelerate business automation, reduce complexity, save costs, and automate regular tasks, companies must use the power of AI.
+
+IBM Cloud Pak for Watson AIOps powers automation by using diverse data sets from an entire range of hybrid environments from cloud to on-premises, and bringing the information together across ITOps. With this Cloud Pak, you can tap into shared automation services to get insight into how your processes run. You can also visualize hotspots and bottlenecks, and pinpoint what to fix with event detection to prioritize which issues to address first.
+
+IBM Cloud Pak for Watson AIOps helps you uncover hidden insights from multiple sources of data, such as logs, metrics, and events. The Cloud Pak delivers those insights directly into the tools that your teams already use, such as Slack or Microsoft Teams, in near real-time. Included AI management tools provides you with unprecedented visibility into your organization's infrastructure so that you can predict failures and facilitate problem resolution.
+
+The IBM Automation foundation that IBM Cloud Pak for Watson AIOps is installed upon is a common foundation of AI and automation that delivers a secure and consistent experience for multiple IBM Cloud Paks. For example, the foundation provides a standard dashboard with a customizable card-based layout that shows key data. It also provides fine-grained role-based access control on menu items and displayed information; and guided tours and tutorials to help users get started.
+
+## Features
+IBM Cloud Pak for Watson AIOps is composed of the following key features:
+
+- Guided tours
+- AI management
+- ChatOps
+- AI models and training
+- Data ingestion
+- Application management
+- Infrastructure automation
+
+These capabilities are supported by an ecosystem of connectors and capabilities that manage all facets of the AIOps lifecycle from model training to execution.
+
+See the [IBM Documentation: Overview of IBM Cloud Pak for Watson AIOps](https://www.ibm.com/docs/en/SSJGDOB_3.4/overview/overview.html) for more information.
+
+IBM Watson AIOps consists of the following core components.
+
+# Details
+
+## Prerequisites
+
+The following prerequisites are required for a successful installation.
+
+- A Red Hat OpenShift Container Platform cluster with persistent storage support. For more information on the cluster requirements, see [IBM Documentation: Planning System Requirements](https://www.ibm.com/docs/en/SSJGDOB_3.3.4/planning/requirements_system.html).
+- Openshift command line interface (`oc`) installed and able to communicate with the cluster. Cluster administrator access is required.
+
+For more information on prerequisites, see IBM Documentation for [online](https://www.ibm.com/docs/en/SSJGDOB_3.4/installing/prerequisites.html) and [airgap](https://www.ibm.com/docs/en/SSJGDOB_3.3.2/installing/prerequisites_airgap.html) deployments.
+
+### Resources Required
+See [IBM Documentation: Hardware requirements](https://www.ibm.com/docs/en/SSJGDOB_3.4/planning/requirements_hware_both.html) for detailed information.
+
+> - You cannot change your selected size after deployment.
+> - Multi-region and multi-zone clusters are not supported.
+
+The minimum cluster requirements for a trial deployment are:
+- Worker node count: 3
+- vCPU: 48
+- Memory (GB): 132
+- CoreOS root disk (GB): 360
+- Persistent storage (Gi): 671
+
+# Installing (Guidance)
+
+Ensure that you understand and have met the requirements explained under the [Prerequisites](#Prerequisites) topic.
+
+Refer to the IBM Documentation for guidance on [IBM Documentation: Installing IBM Cloud Pak for Watson AIOps](https://www.ibm.com/docs/en/SSJGDOB_3.4/installing/installing_aimgr.html).
+
+## Configuration
+
+### Ingress Controller Config
+
+Your OpenShift environment may need to be updated to allow network policies to function correctly. To determine if your OpenShift environment is affected, view the default ingresscontroller and locate the property `endpointPublishingStrategy.type`. If it is set to `HostNetwork`, the network policy will not work against routes unless the default namespace contains the selector label.
+
+```
+kubectl get ingresscontroller default -n openshift-ingress-operator -o yaml
+
+  endpointPublishingStrategy:
+    type: HostNetwork
+```
+To set the label via a patch of the default namespace run:
+
+```
+kubectl patch namespace default --type=json -p '[{"op":"add","path":"/metadata/labels","value":{"network.openshift.io/policy-group":"ingress"}}]'
+```
+
+### Network Policy for traffic between the Operator Lifecycle Manager and the CatalogSource service
+
+To install IBM Cloud Pak for Watson AIOps, the OpenShift Operator Lifecycle Manager (OLM) must be able to communicate with the IBM Cloud Pak for Watson AIOps CatalogSource service. If your OpenShift cluster has network policies that restrict communication, you must create a network policy to allow communication between the OLM and the IBM Cloud Pak for Watson AIOps CatalogSource service.
+
+The following network policy when applied will allows traffic between the IBM Cloud Pak for Watson AIOps installation namespace and all other namespaces.
+Replace <namespace> with the IBM Cloud Pak for Watson AIOps installation namespace.
+
+```
+apiVersion: networking.k8s.io/v1
+kind: NetworkPolicy
+metadata:
+  name: allow-all-egress-and-ingress
+  namespace: <namespace>
+spec:
+  egress:
+  - {}
+  ingress:
+  - {}
+  podSelector: {}
+  policyTypes:
+  - Egress
+  - Ingress
+```
+
+If you require a more restrictive network policy, then you can create a network policy that only allows traffic between the OLM namespace and the IBM Cloud Pak for Watson AIOps installation namespace. For more information see [Red Hat OpenShift Documentation: About Network Policy](https://docs.openshift.com/container-platform/4.8/networking/network_policy/about-network-policy.html).
+
+## Storage
+
+You must create persistent storage prior to your installation of IBM Cloud Pak for Watson AIOps on OpenShift.
+
+For more information on storage requirements and the steps required to set up your storage, see [IBM Documentation: Storage considerations](https://www.ibm.com/docs/en/SSJGDOB_3.4/planning/considerations_storage.html).
+
+
+## SecurityContextConstraints Requirements
+
+This chart requires a SecurityContextConstraints(SCC) to be bound to the target service account groups prior to installation. All associated pods will have access to use this SCC. A SCC constrains the actions a pod can perform.  
+
+The IBM Cloud Pak for Watson AIOps will utilize the Openshift Restricted SCC available out of the box. In addition, the following Custom SCCs are used.
+
+#### Custom SecurityContextConstraints definitions
+
+```
+apiVersion: security.openshift.io/v1
+kind: SecurityContextConstraints
+metadata:
+  name: ibm-noi-scc
+allowHostDirVolumePlugin: false
+allowHostIPC: true
+allowHostNetwork: false
+allowHostPID: false
+allowHostPorts: false
+allowPrivilegeEscalation: true
+allowPrivilegedContainer: false
+defaultAddCapabilities: []
+allowedCapabilities:
+- SETPCAP
+- AUDIT_WRITE
+- CHOWN
+- NET_RAW
+- DAC_OVERRIDE
+- FOWNER
+- FSETID
+- KILL
+- SETUID
+- SETGID
+- NET_BIND_SERVICE
+- SYS_CHROOT
+- SETFCAP
+- IPC_OWNER
+- IPC_LOCK
+- SYS_NICE
+- DAC_OVERRIDE
+priority: 11
+fsGroup:
+  type: RunAsAny
+readOnlyRootFilesystem: false
+requiredDropCapabilities:
+- MKNOD
+runAsUser:
+  type: RunAsAny
+seLinuxContext:
+  type: RunAsAny
+supplementalGroups:
+  type: RunAsAny
+volumes:
+- configMap
+- downwardAPI
+- emptyDir
+- persistentVolumeClaim
+- projected
+- secret
+---
+apiVersion: security.openshift.io/v1
+kind: SecurityContextConstraints
+metadata:
+  name: aiops-restricted
+allowHostDirVolumePlugin: false
+allowHostIPC: false
+allowHostNetwork: false
+allowHostPID: false
+allowHostPorts: false
+allowPrivilegedContainer: false
+allowPrivilegeEscalation: false
+allowedCapabilities: null
+allowedFlexVolumes: null
+allowedUnsafeSysctls: null
+defaultAddCapabilities: null
+defaultAllowPrivilegeEscalation: false
+forbiddenSysctls:
+  - "*"
+fsGroup:
+  type: MustRunAs
+  ranges:
+  - max: 65535
+    min: 1
+readOnlyRootFilesystem: false
+requiredDropCapabilities:
+- ALL
+runAsUser:
+  type: MustRunAsNonRoot
+seccompProfiles:
+- docker/default
+seLinuxContext:
+  type: RunAsAny
+supplementalGroups:
+  type: MustRunAs
+  ranges:
+  - max: 65535
+    min: 1
+volumes:
+- configMap
+- downwardAPI
+- emptyDir
+- persistentVolumeClaim
+- projected
+- secret
+```
+
+## Limitations
+
+* Platform limited, only supports `amd64` worker nodes.
+* For an overview of known issues see the IBM Documentation topic for [IBM Documentation: Known issues and limitations](https://www.ibm.com/docs/en/SSJGDOB_3.4/about/known_issues.html).
+
+## Documentation
+
+* [IBM Documentation: IBM Cloud Pak for Watson AIOps v3.3.2](https://www.ibm.com/docs/en/SSJGDOB_3.4).
---
+++ case/ibm-cp-waiops/1.3.0/ibm-cp-waiops/inventory/cpwaiopsSetup/files/launch-impl.sh
@@ -0,0 +1,300 @@
+# (C) Copyright IBM Corp. 2020  All Rights Reserved.
+#
+# This script implements/overrides the base functions defined in launch.sh
+
+# CPWAIOps operator specific variables
+caseName="ibm-cp-waiops"
+inventory="cpwaiopsSetup"
+caseCatalogName="ibm-cp-waiops-catalog"
+# List of CASE dependencies for recursive catalog source install and uninstall
+case_depedencies="ibm-management-kong \
+ ibm-watson-aiops-ui-operator-case \
+ ibm-watson-ai-manager \
+ ibm-asm-operator-case \
+ ibm-postgreservice \
+ ibm-aiops-ir-core-operator \
+ ibm-aiops-lifecycle \
+ ibm-aiops-analytics \
+ ibm-aiopsedge-bundle \
+ ibm-secure-tunnel \
+ ibm-cp-common-services \
+ ibm-cp-automation-foundation \
+ ibm-couchdb \
+ ibm-cloud-databases-redis \
+ ibm-cloud-native-postgresql"
+recursive_action=1
+
+
+# returns name of the inventory containing setup code, given a CASE name
+# this is used during the install of catalog of dependent CASE
+# this list must be updated as additional items are added to case_dependencies
+# the dependent CASE items must support `install-catalog` and `uninstall-catalog`
+# the echo value must be the name of the CASE inventory for the catalog actions
+dependent_inventory_item() {
+    local case_name=$1
+    case $case_name in
+    ibm-watson-ai-manager)
+        echo "aiManagerSetup"
+        return 0
+        ;;
+    ibm-watson-aiops-ui-operator-case)
+        echo "ibmWatsonAiopsUiOperatorSetup"
+        return 0
+        ;;
+    ibm-management-kong)
+        echo "ibmKongOperatorSetup"
+        return 0
+        ;;
+    ibm-asm-operator-case)
+        echo "asmOperatorSetup"
+        return 0
+        ;;
+    ibm-secure-tunnel)
+        echo "ibmSecureTunnelOperatorSetup"
+        return 0
+        ;;
+    ibm-postgreservice)
+        echo "ibmPostgreserviceOperatorSetup"
+        return 0
+        ;;
+    ibm-aiops-ir-core-operator)
+        echo "issueResolutionCoreOperatorSetup"
+        return 0
+        ;;
+    ibm-aiops-lifecycle)
+        echo "aiopsLifecycleOperatorSetup"
+        return 0
+        ;;
+    ibm-aiops-analytics)
+        echo "aiopsAnalyticsOperatorSetup"
+        return 0
+        ;;
+    ibm-aiopsedge-bundle)
+        echo "aiopsedge"
+        return 0
+        ;;
+    ibm-cp-common-services)
+        echo "ibmCommonServiceOperatorSetup"
+        return 0
+        ;;
+    ibm-cp-automation-foundation)
+        echo "iafOperatorSetup"
+        return 0
+        ;;
+    ibm-couchdb)
+        echo "couchdbOperatorSetup"
+        return 0
+        ;;
+    ibm-cloud-databases-redis)
+        echo "redisOperator"
+        return 0
+        ;;
+    ibm-cloud-native-postgresql)
+        echo "ibmCloudNativePostgresqlOperator"
+        return 0
+        ;;
+    *)
+        echo "unknown case: $case_name"
+        return 1
+        ;;
+    esac
+}
+
+# returns a case tgz given a dependency name
+dependent_case_tgz() {
+    local case_name=$1
+    local input_dir=$2
+
+    # if there are multiple versions of the case is downloaded ( this happens when same dependency
+    # is requested by a different case but with a different version)
+    # use the latest version
+    # the below command finds files that start with dependent case name, sorts by semver field
+    # note that this sort flag is only available on GNU sort ( linux versions)
+    case_tgz=$(find "${input_dir}" -name "${case_name}*.tgz" | sort --reverse --version-sort --field-separator="-" | head -n1)
+
+    if [[ -z ${case_tgz} ]]; then
+        err_exit "failed to find case tgz for dependent case: ${case_name}"
+    fi
+
+    echo "${case_tgz}"
+}
+
+# ----- INSTALL ACTIONS -----
+
+# Installs the catalog source including dependent catalogs
+install_dependent_catalogs() {
+
+    local dep_case=""
+
+    for dep in $case_depedencies; do
+        local dep_case="$(dependent_case_tgz "${dep}" "${inputcasedir}")"
+
+        echo "-------------Installing dependent catalog source: ${dep_case}-------------"
+
+        validate_file_exists "${dep_case}"
+        local inventory=""
+        inventory=$(dependent_inventory_item "${dep}")
+
+        cloudctl case launch \
+            --case "${dep_case}" \
+            --namespace "${namespace}" \
+            --inventory "${inventory}" \
+            --action install-catalog \
+            --args "--registry ${registry} --inputDir ${inputcasedir} --recursive ${dryRun:+--dryRun }" \
+            --tolerance "${tolerance_val}"
+
+        if [[ $? -ne 0 ]]; then
+            err_exit "installing dependent catalog for '${dep_case}' failed"
+        fi
+    done
+}
+
+# Installs the catalog source including dependent catalogs
+install_catalog() {
+
+    validate_install_catalog
+
+    # install all catalogs of subcases first
+    if [[ ${recursive_action} -eq 1 ]]; then
+        install_dependent_catalogs
+    fi
+
+    echo "-------------Installing catalog source-------------"
+
+    local catsrc_file="${casePath}/inventory/${inventory}/files/catalog_source.yaml"
+
+    # Verfy expected yaml files for install exit
+    validate_file_exists "${catsrc_file}"
+
+    # Apply yaml files manipulate variable input as required
+    if [[ -z $registry ]]; then
+        # If an additional arg named registry is NOT passed in, then just apply
+        tee >($kubernetesCLI apply ${dryRun} -f -) < "${catsrc_file}"
+    else
+        # If an additional arg named registry is passed in, then adjust the name of the image and apply
+        local catsrc_image_orig=$(grep "image:" "${catsrc_file}" | awk '{print$2}')
+
+        # replace original registry with local registry
+        local catsrc_image_mod="${registry}/$(echo "${catsrc_image_orig}" | sed -e "s/[^/]*\///")"
+
+        # apply catalog source
+        sed -e "s|${catsrc_image_orig}|${catsrc_image_mod}|g" "${catsrc_file}" | tee >($kubernetesCLI apply ${dryRun} -f -) | cat
+    fi
+
+    echo "done"
+
+}
+
+# ----- UNINSTALL ACTIONS -----
+
+uninstall_dependent_catalogs() {
+
+    local dep_case=""
+
+    for dep in $case_depedencies; do
+        local dep_case="$(dependent_case_tgz "${dep}" "${inputcasedir}")"
+        echo "-------------Uninstalling dependent catalog source: ${dep_case}-------------"
+
+        validate_file_exists "${dep_case}"
+        local inventory=""
+        inventory=$(dependent_inventory_item "${dep}")
+
+        cloudctl case launch \
+            --case "${dep_case}" \
+            --namespace "${namespace}" \
+            --inventory "${inventory}" \
+            --action uninstall-catalog \
+            --args "--recursive --inputDir ${inputcasedir} ${dryRun:+--dryRun }" \
+            --tolerance "${tolerance_val}"
+
+        if [[ $? -ne 0 ]]; then
+            err_exit "Uninstalling dependent catalog source: ${dep_case} failed"
+        fi
+
+    done
+}
+
+# deletes the catalog source and operator group
+uninstall_catalog() {
+
+    validate_install_catalog "uninstall"
+
+    # uninstall all catalogs of subcases first
+    if [[ ${recursive_action} -eq 1 ]]; then
+        uninstall_dependent_catalogs
+    fi
+
+    local catsrc_file="${casePath}/inventory/${inventory}/files/catalog_source.yaml"
+
+    echo "-------------Uninstalling catalog source-------------"
+    $kubernetesCLI delete -f "${catsrc_file}" --ignore-not-found=true ${dryRun}
+}
+
+# create the subscription resource to install the operator using the OLM method
+install_operator() {
+  echo "-------------Installing operator-------------"
+
+  local subscription_file="${casePath}/inventory/${inventory}/files/subscription.yaml"
+
+  # Verfy expected yaml files for install exit
+  validate_file_exists "${subscription_file}"
+
+  # Apply yaml files manipulate variable input as required
+  if [[ -z $channelName ]]; then
+      # If an additional arg named channelName is NOT passed in, then just apply
+      tee >($kubernetesCLI apply ${dryRun} -f -) < "${subscription_file}"
+  else
+      # If an additional arg named channelName is passed in, then adjust the name of the channel and apply
+      local subscription_channel_orig=$(grep "channel:" "${subscription_file}" | awk '{print$2}')
+
+      # replace original subscription channel with argument
+      local subscription_channel_mod="${channelName}"
+
+      # apply catalog source
+      sed -e "s|${subscription_channel_orig}|${channelName}|g" "${subscription_file}" | tee >($kubernetesCLI apply ${dryRun} -f -) | cat
+  fi
+
+  echo "done"
+
+}
+
+install_gitops_applicationset() {
+
+    echo "-------------Install Gitops ApplicationSet-------------"
+
+    HOSTNAME=$(hostname)
+    local storageclass=rook-cephfs
+    local storageclassblock=rook-cephfs
+    local registry=v2
+    local username=admin
+    local password=admin
+    sed -i 's|HOSTNAME|'"${HOSTNAME}"'|g' "${casePath}"/inventory/"${inventory}"/files/gitops/application.yaml
+    sed -i 's|STORAGECLASS|'"${storageclass}"'|g' "${casePath}"/inventory/"${inventory}"/files/gitops/application.yaml
+    sed -i 's|STORAGECLASSBLOCK|'"${storageclassblock}"'|g' "${casePath}"/inventory/"${inventory}"/files/gitops/application.yaml
+    sed -i 's|REGISTRY|'"${registry}"'|g' "${casePath}"/inventory/"${inventory}"/files/gitops/application.yaml
+    sed -i 's|USERNAME|'"${username}"'|g' "${casePath}"/inventory/"${inventory}"/files/gitops/application.yaml
+    sed -i 's|PASSWORD|'"${password}"'|g' "${casePath}"/inventory/"${inventory}"/files/gitops/application.yaml
+    $kubernetesCLI apply -f "${casePath}"/inventory/"${inventory}"/files/gitops/application.yaml
+
+}
+
+install_gitops_application() {
+
+    echo "-------------Add Cluster to Argocd-------------"
+
+    OCP_CLUSTER_NAME=$($kubernetesCLI config current-context)
+    echo y | argocd cluster add ${OCP_CLUSTER_NAME} --name ocp-$(date +%s)
+
+    echo "done"
+
+}
+
+launch_boot_cluster() {
+
+    echo "-------------Launch Boot Cluster-------------"
+
+    "${casePath}"/inventory/"${inventory}"/files/gitops/install.sh up
+    install_gitops_applicationset
+    echo "done"
+
+}
